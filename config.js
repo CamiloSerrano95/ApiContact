19e555db0662b9c52804ead0b768fdc60836c141
Unknown <casegica@gmail.com>
2018-10-16 23:26:38 -0500

    Correcion user DB

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: process.env.MONGODB || 'mongodb://Camilo Serrano:ca1067943114*-+@ds135003.mlab.com:35003/contact',
+    db:  process.env.MONGODB || 'mongodb://camiloserrano95:ca1067943114*-+@ds135003.mlab.com:35003/contact',
     SECRET_TOKEN: process.env.MONGODB || 'contact-token-secret'
 }
